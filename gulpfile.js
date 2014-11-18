1732ac3e8eba188d27a15f0585f72cff69ce860d
Michael Cavalea <callmecavs@gmail.com>
2014-11-17 19:57:11 -0500

    gulpfile cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
-// init gulp
+// gulp
 var gulp   = require('gulp');
 
-// init plugins
+// plugins
 var plumber = require('gulp-plumber');
 var sass    = require('gulp-sass');
 var concat  = require('gulp-concat');
@@ -15,23 +15,19 @@ var onError = function(error) {
   console.log(error);
 }
 
-// compile sass, without source map
+// compile sass
 gulp.task('sass', function() {
   return gulp.src('style/style.scss')
-    .pipe(plumber({
-      errorHandler: onError
-    }))
+    .pipe(plumber({ errorHandler: onError }))
     .pipe(sass({ outputStyle: 'compressed' }))
     .pipe(gulp.dest('./'))
     .pipe(notify('SASS compiled.'))
 });
 
-// concatenate and uglify scripts
+// concat and uglify scripts
 gulp.task('scripts', function() {
   return gulp.src('scripts/*.js')
-    .pipe(plumber({
-      errorHandler: onError
-    }))
+    .pipe(plumber({ errorHandler: onError }))
     .pipe(concat('scripts.js'))
     .pipe(rename('scripts.min.js'))
     .pipe(uglify())
@@ -39,7 +35,7 @@ gulp.task('scripts', function() {
     .pipe(notify('JS concatenated and uglified.'));
 });
 
-// start up server
+// spin up server
 gulp.task('server', function() {
   return connect.server({
     root: './',
