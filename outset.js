a275e435c9f7eefbf4112f3bcea30e0f7c6116e7
Michael Cavalea <callmecavs@gmail.com>
2015-03-10 13:35:09 -0400

    fixed the .gitignore renaming issue, for real this time

--- outset.js
+++ outset.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
+
 'use strict';
 
 var fs = require('fs-extra');
 
+var CWD  = process.cwd();
 var FROM = __dirname + '/lib';
-var TO   = process.cwd();
 
-fs.copy(FROM, TO, function(error) {
+fs.copy(FROM, CWD, function(error) {
   if(error) console.log(error);
+
+  // rename .gitignore
+  fs.renameSync(CWD + '/gitignore', CWD + '/.gitignore');
 });
