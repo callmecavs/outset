385e14a0402558a18617a77b1fb2e9cf77152d77
Michael Cavalea <callmecavs@gmail.com>
2016-02-18 10:29:30 -0500

    update bin script

--- outset.js
+++ outset.js
@@ -6,8 +6,8 @@ const fs = require('fs-extra')
 
 const path = process.argv[2] || '.'
 
-const TO   = `${ process.cwd() }/${ path }`
 const FROM = `${ __dirname }/lib`
+const TO   = `${ process.cwd() }/${ path }`
 
 fs.copy(FROM, TO, (error) => {
   if(error) return console.log(error)
